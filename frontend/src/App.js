ab763259e757964ea2994c017aa3149e36426ad5
ksotis <ks.srikittichai@gmail.com>
2021-07-30 16:18:38 +0700

    add test data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const API_ENDPOINT = 'http://localhost:4000'
 
 function App() {
+  const [data, setData] = useState()
+  useEffect(() => {
+    fetch(`${API_ENDPOINT}/`)
+      .then(resp => resp.json())
+      .then(resp => setData(resp))
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        {JSON.stringify(data, null, 2)}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
